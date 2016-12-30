6c7f8f00eebbb68f1f02c551e8936564cf2e69b9
gaigai <gracegaiyue@gmail.com>
2016-12-30 15:28:58 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :groups
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
   has_many :posts
@@ -11,4 +12,13 @@ class User < ApplicationRecord
   def is_member_of?(group)
     participated_groups.include?(group)
   end
+  
+  def join!(group)
+  participated_groups << group
+  end
+
+  def quit!(group)
+  participated_groups.delete(group)
+  end
+
 end
